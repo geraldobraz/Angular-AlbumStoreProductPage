95d80ae9cf9353b324a0f16c6c047b3b533eabc5
geraldobraz <g.de.almeida.neto@avanade.com>
2019-09-07 09:47:57 -0300

    Solving the Module2 errors.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ProductPageComponent,
+      ProductDescriptionComponent,
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
