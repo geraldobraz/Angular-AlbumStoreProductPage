72356a73c756a92668b26a525570c4d5e4233592
geraldobraz <g.de.almeida.neto@avanade.com>
2019-09-07 11:10:05 -0300

    - Service Creation - Getting Page info from http response

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-
-  constructor() { }
+  albumInfo: any
+  
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    console.log('NgInit Description');
+    this._productService.getAlbum(1).subscribe(response => {
+      console.log('Response:', response)
+      this.albumInfo = response;
+    });
+    
   }
 
 }
